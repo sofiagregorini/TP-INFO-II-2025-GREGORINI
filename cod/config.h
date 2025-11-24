865e9ed34c13c092444ca794b6ca94722a415f4a
sofiagregorini <102880962+sofiagregorini@users.noreply.github.com>
2025-11-24 01:13:53 -0300

    Add configuration header for temperature sensor and controls

---
+++ cod/config.h
@@ -0,0 +1,12 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+
+#define PIN_SENSOR_TEMPERATURA 0    // Canal ADC para sensor de temperatura
+#define PIN_BOTON 6                 // Pin del puerto D para el botón
+#define PIN_VENTILADOR 7            // Pin del puerto D para el ventilador
+#define TEMPERATURA_LIMITE 90.0     // Temperatura límite en grados Celsius
+
+
+
+#endif
