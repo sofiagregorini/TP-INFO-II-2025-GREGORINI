88c08ca8677c0f99f8fa84d3ed9e1e308f766f13
sofiagregorini <102880962+sofiagregorini@users.noreply.github.com>
2025-11-28 15:24:00 -0300

    Add new states to estado_t enum
    Added new states for luminosity monitoring, emergency activation, and progressive shutdown.

--- cod/estados.h
+++ cod/estados.h
@@ -3,19 +3,19 @@
 
 // Definición de estados del sistema
 typedef enum {
-    INIT,
-    ESPERA,
-    ENFRIANDO,
+    ESPERA_LUMINOSIDAD,        // Estado 1: espera monitoreo de luminosidad ambiental luz apagada
+    ACTIVACION_EMERGENCIA,     // Estado 2: activacion de emergencia luz encendida
+    APAGADO_PROGRESIVO,        // Estado 3: apagado progresivo de la luz
     ERROR
 } estado_t;
 
 // Definición del tipo de puntero a función para estados
 typedef estado_t (*funcion_estado_t)(void);
 
-// Prototipos de funciones de estados
-estado_t estado_init(void);
-estado_t estado_espera(void);
-estado_t estado_enfriando(void);
+// Prototipos de funciones 
+estado_t estado_espera_luminosidad(void);
+estado_t estado_activacion_emergencia(void);
+estado_t estado_apagado_progresivo(void);
 estado_t estado_error(void);
 
 #endif
