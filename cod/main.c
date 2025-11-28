05927abad9752d2ff53b9e3170a03da219bf4e78
sofiagregorini <102880962+sofiagregorini@users.noreply.github.com>
2025-11-28 15:24:57 -0300

    Refactor state machine to include new states

--- cod/main.c
+++ cod/main.c
@@ -1,26 +1,26 @@
 #include "estados.h"
 #include "config.h"
-#include "hardware.h"
+#include "driver.h"
 #include <stdint.h>
 
 // Vector de punteros a funciones de estado
 funcion_estado_t maquina_estados[] = {
-    estado_init,      // INIT
-    estado_espera,    // ESPERA
-    estado_enfriando, // ENFRIANDO
-    estado_error      // ERROR
+    estado_espera_luminosidad,    // ESPERA_LUMINOSIDAD
+    estado_activacion_emergencia, // ACTIVACION_EMERGENCIA
+    estado_apagado_progresivo,    // APAGADO_PROGRESIVO
+    estado_error                  // ERROR
 };
 
 int main(void) {
-    estado_t estado_actual = INIT;
-    
+    estado_t estado_actual = ESPERA_LUMINOSIDAD;
+
    
     cargar_configuracion();
-    
+
     while (1) {
         // Ejecutar la función correspondiente al estado actual
         estado_actual = maquina_estados[estado_actual]();
     }
-    
+
     return 0;
 }
